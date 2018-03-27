0753e151cf5ca2a79dd25fb03a72824701cd4ca6
bramvanlaere <r0625771@student.thomasmore.be>
2018-03-27 11:27:30 +0200

    Image Upload + feed + databank update
    - feed is nog niet afhankelijk van friendlist (hardcoded)
    - recrop wanneer image wordt geupload wordt nu in css gedaan moet in
    php gebeuren
    - feed is nog niet gelimiteerd op 20 posts

--- signup.php
+++ signup.php
@@ -18,10 +18,17 @@ if( !empty($_POST) ) {
         } else{
             $feedback = "Something went wrong.";
         }
+<<<<<<< HEAD
+
+    }
+
+}
+=======
         
     }
 
  }
+>>>>>>> origin/master
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,6 +43,60 @@ if( !empty($_POST) ) {
 
 </head>
 <body>
+<<<<<<< HEAD
+<div class="container">
+    <h2>Register</h2>
+    <div class="feedback">
+        <!--<p><?php echo $feedback; ?></p>-->
+    </div>
+    <form action="" method="POST">
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="email">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password" name="password">
+                <small id="passwordHelpBlock" class="form-text text-muted">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                </small>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Confirm Password</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Confirm password" name="password_confirmation">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="inputFirstname3" class="col-sm-2 col-form-label">Firstname</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputFirstname3" placeholder="Firstname" name="firstname">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="inputLastname3" class="col-sm-2 col-form-label">lastname</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputLastname3" placeholder="Lastname" name="lastname">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Sign Up</button>
+            </div>
+        </div>
+
+
+    </form>
+=======
     <div class="container">
         <h2>Register</h2>
         <div class="feedback">
@@ -88,6 +149,7 @@ if( !empty($_POST) ) {
 
 
         </form>
+>>>>>>> origin/master
 </div>
 
 
