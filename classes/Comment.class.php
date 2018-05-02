e9362863329d4292ab08d5b34c912b76b4864b92
ThomasL96 <r0488238@student.thomasmore.be>
2018-05-02 15:45:05 +0200

    fout gebeurd tijdens het pushen, is nu opgelost

--- classes/Comment.class.php
+++ classes/Comment.class.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 include_once ('Db.class.php');
 class Comments
@@ -41,50 +41,11 @@ class Comments
 
 
 
-}
-=======
-<?php
-include_once ('Db.class.php');
-class Comments
-{
-    private $comments;
-    /**
- * @return mixed
- */
-    public function getComments()
-    {
-    return $this->comments;
-    }/**
- * @param mixed $comments
- */
-    public function setComments($comments)
-    {
-    $this->comments = $comments;
-    }
-
-    public function saveComments()
-    {
-
-            $conn = Db::getInstance();
-            $statement = $conn->prepare("insert into comments (comment,user,commentuserid) values (:comment,:user,:commentuserid)");
-            $statement->bindParam(":comment", $this->comments);
-            $statement->bindValue(":commentuserid", $_SESSION['userid']);
-            $statement->bindParam(":user",$_SESSION['user']);
-            $res = $statement->execute();
-            if ($res = true) {
-                header('Location:index.php');
-            }
-
-
-        }
-
-
-
 
 
 
 
 
 }
->>>>>>> 3fea1da4c7a603405dae8e1adebd69080574456d
+
 ?>
